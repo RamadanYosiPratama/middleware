ce5484023ab667e12bf106052f094ab8e2ec8221
RamadanYosiPratama <ramadanyosi30@gmail.com>
2019-12-05 15:44:02 +0700

    middleware

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+
+
+const app = express()
+app.set('view engine','ejs')
+app.set('views','views')
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended : true
+})
+)
+app.get('/',(req, res) => {
+    res.render('login')
+})
+
+require('./router/router')(app)
+
+app.listen(8000,() => {
+    console.log('Berhasil')
+})
